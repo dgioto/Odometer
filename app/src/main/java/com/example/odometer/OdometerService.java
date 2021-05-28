c2c834be92485076c677cb8c16467e4fe26c3564
Alexander Tih <dgioto@gmail.com>
2021-05-28 10:12:30 +0300

    использование службы позиционирования Android

--- app/src/main/java/com/example/odometer/OdometerService.java
+++ app/src/main/java/com/example/odometer/OdometerService.java
@@ -2,8 +2,13 @@ package com.example.odometer;
 
 import android.app.Service;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
+
+import androidx.annotation.NonNull;
 
 import java.util.Random;
 
@@ -12,6 +17,8 @@ public class OdometerService extends Service {
     private final IBinder binder = new OdometerBinder();
     private final Random random = new Random();
 
+    private LocationListener listener;
+
     public OdometerService() {
     }
 
@@ -19,6 +26,34 @@ public class OdometerService extends Service {
         OdometerService getOdometer(){
             return OdometerService.this;
         }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        listener = new LocationListener() {
+            @Override
+            //Параметр Location описывает текущее местоположение
+            public void onLocationChanged(@NonNull Location location) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(@NonNull String provider) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(@NonNull String provider) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+        };
     }
 
     @Override
