7e58f008d613e0a60f4f90591102a5d862f4b3ea
Alexander Tih <dgioto@gmail.com>
2021-06-15 10:42:48 +0300

    fix

--- app/src/main/java/com/example/odometer/fragments/HistoryFragment.java
+++ app/src/main/java/com/example/odometer/fragments/HistoryFragment.java
@@ -1,4 +1,4 @@
-package com.example.odometer;
+package com.example.odometer.fragments;
 
 import android.os.Bundle;
 
@@ -9,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+
+import com.example.odometer.R;
 
 import java.util.List;
 
