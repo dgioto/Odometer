9453f4cb2dc7740cc0736cdc1f90921144b25908
Alexander Tih <dgioto@gmail.com>
2021-07-07 23:20:57 +0300

    create DbManager

--- app/src/main/java/com/example/odometer/db/DbConstants.java
+++ app/src/main/java/com/example/odometer/db/DbConstants.java
@@ -6,13 +6,13 @@ public class DbConstants {
     public static final String TITLE = "title";
     public static final String METERS = "meters";
     public static final String TIMES = "times";
-    public static final String DESCRIPTION = "description";
+    public static final String DESC = "desc";
     public static final String DB_NAME = "odometer_db.db";
     public static final int DB_VERSION = 1;
 
     public static final String TABLE_STRUCTURE = "CREATE TABLE IF NOT EXISTS" +
             TABLE_NAME + " (" + _ID + "INTEGER PRIMARY KEY," + TITLE + " TEXT, " +
-            METERS + " INTEGER, " + TIMES + " TEXT, "+ DESCRIPTION + " TEXT)";
+            METERS + " INTEGER, " + TIMES + " TEXT, "+ DESC + " TEXT)";
 
     public static final String DROP_TABLE = "DROP TABLE IF EXISTS" + TABLE_NAME;
 }
---
+++ app/src/main/java/com/example/odometer/db/DbManager.java
@@ -0,0 +1,51 @@
+package com.example.odometer.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DbManager {
+    private final Context context;
+    private final DbHelper dbHelper;
+    private SQLiteDatabase db;
+
+    public DbManager(Context context) {
+        this.context = context;
+        dbHelper = new DbHelper(context);
+    }
+
+    public void openDb(){
+        db = dbHelper.getWritableDatabase();
+    }
+
+    public void insertToDb(String title, int meters, String times, String desc){
+        ContentValues cv = new ContentValues();
+        cv.put(DbConstants.TITLE, title);
+        cv.put(DbConstants.METERS, meters);
+        cv.put(DbConstants.TIMES, times);
+        cv.put(DbConstants.DESC, desc);
+        db.insert(DbConstants.TABLE_NAME, null, cv);
+    }
+
+    public List<String> getFromDb(){
+        List<String> tempList = new ArrayList<>();
+        Cursor cursor = db.query(DbConstants.TABLE_NAME, null,
+                null, null, null, null, null);
+
+        while (cursor.moveToNext()){
+            String title = cursor.getString(cursor.getColumnIndex(DbConstants.TITLE));
+            tempList.add(title);
+        }
+        cursor.close();
+        return  tempList;
+    }
+
+    public void closeDb(){
+        dbHelper.close();
+    }
+}
+
