15a6749108898bf0c5e83920097e8d2ee8cae8a0
Alexander Tih <dgioto@gmail.com>
2021-07-12 00:03:05 +0300

    update HistoryFragment (it do not work, an error is in a rcView)

--- app/src/main/java/com/example/odometer/HistoryFragment.java
+++ app/src/main/java/com/example/odometer/HistoryFragment.java
@@ -13,14 +13,16 @@ import android.view.ViewGroup;
 
 import com.example.odometer.adapter.MainAdapter;
 import com.example.odometer.db.DbHelper;
+import com.example.odometer.db.DbManager;
 
 public class HistoryFragment extends ListFragment {
 
     private RecyclerView rcView;
     private MainAdapter mainAdapter;
     MainActivity mainActivity;
+    View layout;
 
-    DbHelper myDb;
+    DbManager dbManager;
 
     public HistoryFragment(MainActivity _mainActivity){
         this.mainActivity = _mainActivity;
@@ -29,18 +31,22 @@ public class HistoryFragment extends ListFragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        init();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        layout = inflater.inflate(R.layout.fragment_history, container, false);
+
+        init();
+
+        return layout;
     }
 
     private void init() {
-        rcView = rcView.findViewById(R.id.rcView);
+        dbManager = new DbManager(mainActivity);
+        rcView = layout.findViewById(R.id.rcView);
         mainAdapter = new MainAdapter(mainActivity);
         rcView.setLayoutManager(new LinearLayoutManager(mainActivity));
         rcView.setAdapter(mainAdapter);
@@ -49,6 +55,19 @@ public class HistoryFragment extends ListFragment {
     @Override
     public void onResume() {
         super.onResume();
-//        mainAdapter.updateAdapter(myDb.getReadableDatabase());
+
+        dbManager.openDb();
+        mainAdapter.updateAdapter(dbManager.getFromDb());
+    }
+
+    public void onClickSave(View view){
+        dbManager.insertToDb("Test", 10, "Test", "Test");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        dbManager.closeDb();
     }
 }
