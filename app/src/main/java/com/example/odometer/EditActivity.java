0b46a30c5ea4ef932d2f227ce507d53475df7d42
Alexander Tih <dgioto@gmail.com>
2021-07-21 22:39:55 +0300

    add to a EditActivity Toolbar

--- app/src/main/java/com/example/odometer/EditActivity.java
+++ app/src/main/java/com/example/odometer/EditActivity.java
@@ -1,6 +1,7 @@
 package com.example.odometer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -23,6 +24,9 @@ public class EditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         init();
         getMyIntents();
