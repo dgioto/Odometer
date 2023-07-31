87eae3fdb7f8333082a97bd9b610b1c0be07b191
dgioto <dgioto@gmail.com>
2023-07-31 22:06:10 +0300

    adding stop button to Notification

--- app/src/main/java/com/dgioto/odometer/Service/NotificationService.java
+++ app/src/main/java/com/dgioto/odometer/Service/NotificationService.java
@@ -1,6 +1,7 @@
 package com.dgioto.odometer.Service;
 
 import android.Manifest;
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -21,11 +22,37 @@ public class NotificationService extends Service {
 
     public static final String EXTRA_MESSAGE = "message";
     public static final int NOTIFICATION_ID = 5453;
+    public static final String ACTION_STOP_SERVICE = "com.dgioto.odometer.ACTION_STOP_SERVICE";
+    public static final String CHANNEL_ID = "channel_id";
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         String text = intent.getStringExtra(EXTRA_MESSAGE);
         showText(text);
+
+        // Create the notification channel for Android 8.0 and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Foreground Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+
+        // Make sure the channel ID matches the one used in the NotificationCompat.Builder
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_baseline_directions_run_24)
+                .setContentTitle("Run Tracker")
+                .setContentText(text)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .addAction(R.drawable.ic_stop, "Stop", stopServicePendingIntent())
+                .build();
+
+        startForeground(NOTIFICATION_ID, notification);
+
         return START_STICKY;
     }
 
@@ -35,13 +62,14 @@ public class NotificationService extends Service {
                 this,
                 0,
                 actionIntent,
-                PendingIntent.FLAG_MUTABLE);
+                PendingIntent.FLAG_IMMUTABLE);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "channel_id")
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_baseline_directions_run_24)
                 .setContentText(text)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(actionPendingIntent);
+                .setContentIntent(actionPendingIntent)
+                .addAction(R.drawable.ic_stop, "Stop", stopServicePendingIntent());
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel(
@@ -61,6 +89,16 @@ public class NotificationService extends Service {
                 }
             }
         }
+    }
+
+    private PendingIntent stopServicePendingIntent(){
+        Intent stopServiceIntent = new Intent(this, NotificationService.class);
+        stopServiceIntent.setAction(ACTION_STOP_SERVICE);
+        return PendingIntent.getService(
+                this,
+                0,
+                stopServiceIntent,
+                PendingIntent.FLAG_IMMUTABLE);
     }
 
     @Override
