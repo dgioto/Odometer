8b21846ace3a8d8a15d402787308d706e7dc093e
dgioto <dgioto@gmail.com>
2023-08-15 19:38:10 +0300

    add a close notification button

--- app/src/main/java/com/dgioto/odometer/Service/NotificationService.java
+++ app/src/main/java/com/dgioto/odometer/Service/NotificationService.java
@@ -22,16 +22,26 @@ import com.dgioto.odometer.R;
 public class NotificationService extends Service {
 
     public static final String EXTRA_MESSAGE = "message";
+    public static final String CHANNEL_ID = "channel_id";
+    public static final String CLOSE_NOTIFICATION = "CLOSE_NOTIFICATION";
     public static final int NOTIFICATION_ID = 5453;
     private static final int DELAY_IN_MILLISECONDS = 2000;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        String text = intent.getStringExtra(EXTRA_MESSAGE);
+        if (intent != null) {
+            String text = intent.getStringExtra(EXTRA_MESSAGE);
+            String action = intent.getAction();
 
-        // Trigger a delay before displaying a notification
-        Handler handler = new Handler(Looper.getMainLooper());
-        handler.postDelayed(() -> showText(text), DELAY_IN_MILLISECONDS);
+            if (CLOSE_NOTIFICATION.equals(action)) {
+                stopSelf(); // Close the service after clicking on the "Close" button
+                return START_NOT_STICKY;
+            }
+
+            // Trigger a delay before displaying a notification
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.postDelayed(() -> showText(text), DELAY_IN_MILLISECONDS);
+        }
 
         return START_STICKY;
     }
@@ -44,15 +54,24 @@ public class NotificationService extends Service {
                 actionIntent,
                 PendingIntent.FLAG_IMMUTABLE);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "channel_id")
+        Intent closeIntent = new Intent(this, NotificationService.class);
+        closeIntent.setAction(CLOSE_NOTIFICATION);
+        PendingIntent closePendingIntent = PendingIntent.getService(
+                this,
+                0,
+                closeIntent,
+                PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_baseline_directions_run_24)
                 .setContentText(text)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(actionPendingIntent);
+                .setContentIntent(actionPendingIntent)
+                .addAction(R.drawable.ic_close, getResources().getString(R.string.close), closePendingIntent);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel(
-                    "channel_id",
+                    CHANNEL_ID,
                     "Foreground Service Channel",
                     NotificationManager.IMPORTANCE_DEFAULT);
             ((NotificationManager) getSystemService(NOTIFICATION_SERVICE)).createNotificationChannel(channel);
